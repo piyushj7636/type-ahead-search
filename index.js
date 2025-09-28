d9123767725992db02bf01acd5bc3cea35b85658
piyushj7636 <158646485+piyushj7636@users.noreply.github.com>
2025-09-28 12:39:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+async function fetchData() {
+	const searchInput = document.querySelector(".search").value
+	const result = await fetch(`https://api.duckduckgo.com/?q=${searchInput}&format=json`)
+	const data = await result.json()
+	document.querySelector(".results").textContent = ""
+	const suggestions = data.RelatedTopics.filter(item => "FirstURL" in item).forEach((q) => {
+		const suggestionBox = document.createElement("div")
+		document.querySelector(".results").appendChild(suggestionBox).textContent = q.Text})
+	console.log(suggestions)
+}
+
+const debounce = (fn, delay) => {
+	let timer
+	return function(){
+		context = this
+		args = arguments
+		clearTimeout(timer)
+		timer = setTimeout(()=> {
+			fn.apply(context, arguments)
+		}, delay)
+	}
+}
+
+const getData = debounce(fetchData, 300)
